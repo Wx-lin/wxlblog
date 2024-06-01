cb0eb7310f96d887537a76fda2faf1dd2682ea2e
王薪林 <10655211+wang-xinlinlin@user.noreply.gitee.com>
2024-06-01 15:56:20 +0800

    el-menu一级没有icon

--- src/layout/components/Aside.vue
+++ src/layout/components/Aside.vue
@@ -1,27 +1,36 @@
 <template>
   <div>
-    <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-      <el-sub-menu v-for="(item, key) in menuList" :key="key" :index="key + ''">
-        <template #title>
-          <span> {{ item.meta.name }} </span>
-        </template>
-        <el-menu-item v-for="(value, index) in item.children" :index="'' + key + '-' + index">{{ value.meta.name
-          }}</el-menu-item>
-      </el-sub-menu>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose">
+      <template v-for="(item, key) in menuList">
+        <el-sub-menu v-if="item.children" :key="key" :index="key + ''">
+          <template #title>
+            <span>{{ item.meta.name }}</span>
+          </template>
+          <el-menu-item
+            v-for="(value, index) in item.children"
+            :key="index"
+            :index="'' + key + '-' + index">
+            {{ value.meta.name }}
+          </el-menu-item>
+        </el-sub-menu>
+        <el-menu-item v-else :index="key + ''">
+          {{ item.meta.name }}
+        </el-menu-item>
+      </template>
     </el-menu>
   </div>
 </template>
 
 <script setup>
-import menuList from "@/layout/config.js"
+import menuList from '@/layout/config.js';
 
-function handleOpen() {
+function handleOpen() {}
 
-}
-
-function handleClose() {
-
-}
+function handleClose() {}
 </script>
 
 <style scoped></style>
