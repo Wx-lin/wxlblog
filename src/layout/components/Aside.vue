d0d71c5485de06ef9d1c16ea2acf34d66f161662
王薪林 <10655211+wang-xinlinlin@user.noreply.gitee.com>
2024-06-01 14:40:42 +0800

    1

--- src/layout/components/Aside.vue
+++ src/layout/components/Aside.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
-        <el-menu default-active="2" class="el-menu-vertical-demo">
-          <el-sub-menu index="1">
-            <template #title>
-              <span>Navigator One</span>
-            </template>
-              <el-menu-item index="1-1">item one</el-menu-item>
-          </el-sub-menu>
-        </el-menu>
+    <el-menu default-active="2" class="el-menu-vertical-demo">
+      <el-sub-menu index="1">
+        <template #title>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item index="1-1">item one</el-menu-item>
+      </el-sub-menu>
+    </el-menu>
   </div>
 </template>
 
-<script setup>
-import menuList from "@/router/Aside.js"
-console.log(data);
-</script>
+<script setup></script>
 
 <style scoped></style>
