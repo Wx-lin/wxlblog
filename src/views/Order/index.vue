c10555591e5cfb5a68f40fbd381c2afb2f998c1b
Haubotron <467503806@qq.com>
2024-06-04 17:57:40 +0800

    订单列表请求完成

--- src/views/Order/index.vue
+++ src/views/Order/index.vue
@@ -4,7 +4,7 @@
       <template #header>
         <div class="card-header">订单记录</div>
       </template>
-      <OrderTable />
+      <OrderTable :orderListData="orderListData"/>
       <template #footer>
         <OrderPage />
       </template>
@@ -13,7 +13,16 @@
 </template>
 
 <script setup>
+// 请求数据完成  已传递子组件
 import { getOrderList } from '@/api/orderList.js';
+
+const orderListData = ref([])
+onBeforeMount(orderList);
+
+async function orderList() {
+  const result = await getOrderList();
+  orderListData.value = result.list
+}
 </script>
 
 <style scoped>
